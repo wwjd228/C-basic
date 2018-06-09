a0ee6a2092128bf5ae55db42756ff82bc02982d4
Jungle <wwjd228@gmail.com>
2018-06-10 02:34:24 +0800

    Add end of string.

--- getline/getline.c
+++ getline/getline.c
@@ -9,6 +9,7 @@ ssize_t getline_from_consloe(char *line)
     char c;
     while ( i < LIMIT -1 & (c = getchar()) != '\n' & c != EOF )
         line[i++] = c;
+    line[i] = '\0';
     return i;
 }
 
