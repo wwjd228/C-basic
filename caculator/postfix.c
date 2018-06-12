7549dec3052551b6435d97761e5043bbaa189b57
Jungle <wwjd228@gmail.com>
2018-06-13 02:56:12 +0800

    fix begin with white space.

--- caculator/postfix.c
+++ caculator/postfix.c
@@ -41,66 +41,64 @@ int postfix(char *infix, int position, struct stack *opt)
     int previousOpt = 0;
     struct stack tmp;
     tmp.top = 0;
+    position = skipWhiteSpace(infix, position);
 
-    while (infix[position] != '\0' && infix[position] != '\n') {
+    while (!whiteSpace(infix[position]) || position < strlen(infix) - 1) {
         int i = 0;
         position = skipWhiteSpace(infix, position);
-        while (!whiteSpace(infix[position])) {
-            char operation[MAX_DIGITAL];
-            char operator[2];
-            if (infix[position] == ')') {
-                return position + 1;
-            }
+        char operation[MAX_DIGITAL];
+        char operator[2];
+        if (infix[position] == ')') {
+            return position + 1;
+        }
 
-            // push expression in ()
-            else if (infix[position] == '(') {
-                position++;
-                position = postfix(infix, position, opt);
-                numOfOperation++;
-            }
+        // push expression in ()
+        else if (infix[position] == '(') {
+            position++;
+            position = postfix(infix, position, opt);
+            numOfOperation++;
+        }
 
-            // process operator
-            else if (isOperator(infix[position])) {
-                do {
-                    operator[i++] = infix[position++];
-                } while (isOperator(infix[position]));
-                operator[i] = '\0';
-            }
+        // process operator
+        else if (isOperator(infix[position])) {
+            do {
+                operator[i++] = infix[position++];
+            } while (isOperator(infix[position]));
+            operator[i] = '\0';
+        }
 
-            // push operation
-            else {
-                do {
-                    operation[i++] = infix[position++];
-                } while (!isOperator(infix[position]) && !whiteSpace(infix[position]) && !isSign(infix[position]));
-                operation[i] = '\0';
-                numOfOperation++;
-                push(opt, operation, &((*opt).top));
-            }
+        // push operation
+        else {
+            do {
+                operation[i++] = infix[position++];
+            } while (!isOperator(infix[position]) && !whiteSpace(infix[position]) && !isSign(infix[position]));
+            operation[i] = '\0';
+            numOfOperation++;
+            push(opt, operation, &((*opt).top));
+        }
 
-            i = 0;
-            if (numOfOperation == 2) {
-                if ( previousOpt != 0 && getPriority(operator) > getPriority((*opt).element[previousOpt - 1])) {
-                    // Need temp stack to save poped element.
-                    // Poped until previous opt
-                    while ((*opt).top >= previousOpt) {
-                        char *element = pop(opt, &((*opt).top));
-                        push(&tmp, element, &tmp.top);
-                    }
-
-                    // Push all element back
-                    char *low_priorityOp = pop(&tmp, &tmp.top);
-                    while (tmp.top > 0) {
-                        char *element = pop(&tmp, &tmp.top);
-                        push(opt, element, &((*opt).top));
-                    }
-                    push(opt, operator, &((*opt).top));
-                    push(opt, low_priorityOp, &((*opt).top));
+        if (numOfOperation == 2) {
+            if ( previousOpt != 0 && getPriority(operator) > getPriority((*opt).element[previousOpt - 1])) {
+                // Need temp stack to save poped element.
+                // Poped until previous opt
+                while ((*opt).top >= previousOpt) {
+                    char *element = pop(opt, &((*opt).top));
+                    push(&tmp, element, &tmp.top);
                 }
-                else
-                    push(opt, operator, &((*opt).top));
-                previousOpt = (*opt).top;
-                numOfOperation = 1;
+
+                // Push all element back
+                char *low_priorityOp = pop(&tmp, &tmp.top);
+                while (tmp.top > 0) {
+                    char *element = pop(&tmp, &tmp.top);
+                    push(opt, element, &((*opt).top));
+                }
+                push(opt, operator, &((*opt).top));
+                push(opt, low_priorityOp, &((*opt).top));
             }
+            else
+                push(opt, operator, &((*opt).top));
+            previousOpt = (*opt).top;
+            numOfOperation = 1;
         }
     }
     return position;
