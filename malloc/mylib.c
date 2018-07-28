d2223dce1a12039778e5780683f63d4fcb88f40c
Jungle <wwjd228@gmail.com>
2018-07-28 15:40:37 +0800

    Adjust the location of function.
    Use circular list to keep memory data.
    The memory block will insert into cycle when free() and morecore()
    
    Compile: gcc -std=gnu99 main.c mylib.c

--- malloc/mylib.c
+++ malloc/mylib.c
@@ -3,6 +3,29 @@
 static _header_t base;
 static _header_t *freep = NULL;
 
+void myfree(void *ap)
+{
+    _header_t *bp, *p;
+
+    bp = (_header_t *)ap - 1;
+    for ( p = freep ; !(bp > p && bp < p ->s.ptr) ; p = p->s.ptr )
+        if ( p >= p ->s.ptr && (bp > p || bp < p ->s.ptr) )
+            break;
+
+    if ( bp + bp ->s.size == p ->s.ptr ) {
+        bp ->s.size += p ->s.ptr ->s.size;
+        bp ->s.ptr = p ->s.ptr ->s.ptr;
+    } else
+        bp ->s.ptr = p ->s.ptr;
+    if ( p + p ->s.size == bp ) {
+        p ->s.size += bp ->s.size;
+        p ->s.ptr = bp ->s.ptr;
+    } else
+        p ->s.ptr = bp;
+
+    freep = p;
+}
+
 static inline _header_t *morecore(unsigned nu)
 {
     char *cp, *sbrk(int);
@@ -16,19 +39,6 @@ static inline _header_t *morecore(unsigned nu)
     up ->s.size = nu;
     myfree((void *) (up + 1));
     return freep;
-}
-
-void *mycalloc(unsigned n, unsigned size)
-{
-    unsigned nb = n * size;
-    char *p;
-
-    if ( (p = mymalloc(nb)) != NULL ) {
-        while ( --nb )
-            p[nb] = 0;
-    }
-
-    return (void *)p;
 }
 
 void *mymalloc(unsigned nbytes)
@@ -64,25 +74,16 @@ void *mymalloc(unsigned nbytes)
     }
 }
 
-void myfree(void *ap)
+void *mycalloc(unsigned n, unsigned size)
 {
-    _header_t *bp, *p;
+    unsigned nb = n * size;
+    char *p;
 
-    bp = (_header_t *)ap - 1;
-    for ( p = freep ; !(bp > p && bp < p ->s.ptr) ; p = p->s.ptr )
-        if ( p >= p ->s.ptr && (bp > p || bp < p ->s.ptr) )
-            break;
+    if ( (p = mymalloc(nb)) != NULL ) {
+        while ( --nb )
+            p[nb] = 0;
+    }
 
-    if ( bp + bp ->s.size == p ->s.ptr ) {
-        bp ->s.size += p ->s.ptr ->s.size;
-        bp ->s.ptr = p ->s.ptr ->s.ptr;
-    } else
-        bp ->s.ptr = p ->s.ptr;
-    if ( p + p ->s.size == bp ) {
-        p ->s.size += bp ->s.size;
-        p ->s.ptr = bp ->s.ptr;
-    } else
-        p ->s.ptr = bp;
-
-    freep = p;
+    return (void *)p;
 }
+
