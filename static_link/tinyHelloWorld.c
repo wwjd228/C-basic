40f217d4a30d0a068acb6d5976d4b7816036b10e
Jungle <wwjd228@gmail.com>
2019-08-31 17:42:25 +0800

    Static link with linker script
    Implement the example on the book(Chapter 4)
    https://www.tenlong.com.tw/products/9789861818283

---
+++ static_link/tinyHelloWorld.c
@@ -0,0 +1,25 @@
+char * str = "Hello World!\n";
+
+void print()
+{
+   asm("movq $13, %%rdx \n\t"
+       "movq %0, %%rcx  \n\t"
+       "movq $0, %%rbx  \n\t"
+       "movq $4, %%rax  \n\t"
+       "int  $0x80      \n\t"
+       ::"r"(str):"rdx", "rcx", "rbx");
+}
+
+void exit()
+{
+    asm("movq $42, %%rbx  \n\t"
+        "movq $1 , %%rax  \n\t"
+        "int  $0x80       \n\t"
+        ::);
+}
+
+void nomain(void)
+{
+    print();
+    exit();
+}
